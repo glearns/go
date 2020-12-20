9370d219775fa12b372f207759fd3f2d3c4aa226
geenva <geenva@users.noreply.github.com>
2020-12-20 13:07:34 +0800

    first package :)

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,12 +2,9 @@ package greetings
 
 import "fmt"
 
-let message string
-message = fmt.Sprintf("Hi, %v. Welcome!", name)
-
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
     return message
-}
+} 
